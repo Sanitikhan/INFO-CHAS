5364a95098a5a6d2d0765fba1f255ad9c2481a46
Anaëlle <anaelle.boyer.69@gmail.com>
2025-05-25 18:28:49 +0200

    edit name login page

--- pages/login.php
+++ pages/login.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | Sign up</title>
+    <title>Connexion</title>
     <link rel="stylesheet" href="../public/login.css">
     <link rel="icon" type="image/png" href="../img/logo_fc.png" />
     <!-- Boxicons CSS -->
