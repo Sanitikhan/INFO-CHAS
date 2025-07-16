8a6ccb394195553262bd719ceab2922a7ef5a612
Anaëlle <anaelle.boyer.69@gmail.com>
2025-07-16 19:24:37 +0200

    change title page

--- pages/admin/articles.php
+++ pages/admin/articles.php
@@ -31,7 +31,7 @@ $articles = $pdo->query("
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livraisons</title>
+    <title>Articles</title>
     <link rel="stylesheet" href="../../public/style.css">
     <!--<link rel="stylesheet" href="../../public/form.css">-->
     <link rel="stylesheet" href="../../public/stock.css">
--- pages/admin/lots.php
+++ pages/admin/lots.php
@@ -98,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create_lot'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livraisons</title>
+    <title>Lots</title>
     <link rel="stylesheet" href="../../public/style.css">
     <!--<link rel="stylesheet" href="../../public/form.css">-->
     <link rel="stylesheet" href="../../public/stock.css">
